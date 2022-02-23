844d016ee6d46249a6b01632f716aceadd93c968
Soltan555 <98511484+Soltan555@users.noreply.github.com>
2022-02-23 23:57:50 +0400

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,32 @@
+let one = document.getElementById("one")
+let two = document.getElementById("two")
+let three = document.getElementById("three")
+let four = document.getElementById("four")
+let five = document.getElementById("five")
+let six = document.getElementById("six")
+
+
+one.innerHTML = "Soltan Abdurahmanov"
+two.innerHTML = "Soltan Abdurahmanov"
+three.innerHTML = "Soltan Abdurahmanov"
+four.innerHTML = "Soltan Adurahmanov"
+five.innerHTML = "Soltan Abdurahmanov"
+six.innerHTML = "Soltan Abdurahmanov"
+
+
+
+
+console.log("name");
+
+let name = "Soltan"
+let surname = "Abdurahmanov"
+
+let fullName = name + " " + surname
+
+let ekran = document.getElementById(`test`)
+
+ekran.innerHTML = fullName
+
+let text = document.getElementById(`text`)
+
+text.innerHTML = fullName
